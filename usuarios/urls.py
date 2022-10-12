a73ba5c6616fd15fcbf85368e4566b678c0eb9ab
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-10-12 17:48:30 -0300

    correção

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path(r'cadastro/', views.cadastro, name='cadastro'),
     path(r'login/', views.login, name='login'),
     path(r'home/', views.home, name='home'),
-    path(r'/', views.home, name='home')
+    path(r'', views.home, name='home')
 
 ]
